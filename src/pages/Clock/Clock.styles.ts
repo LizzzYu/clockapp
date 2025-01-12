01724e25b07b5bcf44242739c0d184c8046d5911
Liz <qqqq929@hotmail.com>
2025-01-12 20:36:16 +0800

    refactor: extract styles from Clock component

---
+++ src/pages/Clock/Clock.styles.ts
@@ -0,0 +1,123 @@
+import { motion } from 'framer-motion';
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Breakpoints } from '../../constants/breakpoints.enum';
+
+export const Wrapper = styled.div`
+  ${({ theme }) => theme.mixins.flexCenter}
+  width: 100%;
+  flex-direction: column;
+`;
+
+export const ClocksWrapper = styled.div`
+  ${({ theme }) => theme.mixins.flexCenter}
+  width: 100%;
+  flex-direction: column;
+  margin: 0 53px;
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    gap: 40px;
+    flex-direction: row;
+  }
+`;
+
+export const ClockWrapper = styled.div`
+  position: relative;
+  margin-top: 40px;
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
+    margin-top: 50px;
+  }
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    margin-top: 80px;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const EditIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 40px;
+  right: 24px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.green};
+  }
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    right: 20%;
+  }
+`;
+
+export const LabelsContainer = styled.div`
+  ${({ theme }) => theme.mixins.flexCenter}
+  flex-direction: column;
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
+    padding-top: ${({ theme }) => theme.spacing(2)};
+  }
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    padding-top: ${({ theme }) => theme.spacing(4)};
+  }
+`;
+
+export const TimeLabel = styled.h3`
+  padding: 0;
+  margin: 0;
+  font-size: 40px;
+  font-weight: 600;
+`;
+
+export const CityLabel = styled.h6`
+  padding: 0;
+  margin: 0;
+  font-size: 16px;
+  font-weight: 300;
+  color: ${({ theme }) => theme.colors.green};
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    max-width: 30%;
+  }
+`;
+
+export const LocationIcon = styled(motion.img)`
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  top: -30px;
+  left: 50%;
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
+    width: 40px;
+    height: 40px;
+    top: -40px;
+  }
+
+  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
+    width: 56px;
+    height: 56px;
+    top: -70px;
+  }
+`;
+
+export const locationIconVariants = {
+  initial: { x: '-50%', y: -20, opacity: 0 },
+  animate: {
+    x: '-50%',
+    y: [0, -5, 0],
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: 'easeInOut',
+    },
+  },
+};
--- src/pages/Clock/Clock.tsx
+++ src/pages/Clock/Clock.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
-import styled from '@emotion/styled';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence } from 'framer-motion';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 import { RootState } from '../../redux/store';
 import { logoutAction } from '../../redux/authSlice';
@@ -11,128 +9,20 @@ import useResponsiveSize from '../../utils/useResponsiveSize ';
 import ClockVisual from '../../components/ClockVisual/ClockVisual';
 import Button from '../../components/Button/Button';
 import { PagesRoutes } from '../../constants/pages.enum';
-import { Breakpoints } from '../../constants/breakpoints.enum';
 import { getFormattedTime } from '../../utils/getFormattedTime';
 import Location from '/location.svg';
-
-const Wrapper = styled.div`
-  ${({ theme }) => theme.mixins.flexCenter}
-  width: 100%;
-  flex-direction: column;
-`;
-
-const ClocksWrapper = styled.div`
-  ${({ theme }) => theme.mixins.flexCenter}
-  width: 100%;
-  flex-direction: column;
-  margin: 0 53px;
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    gap: 40px;
-    flex-direction: row;
-  }
-`;
-
-const ClockWrapper = styled.div`
-  position: relative;
-  margin-top: 40px;
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
-    margin-top: 50px;
-  }
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    margin-top: 80px;
-  }
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const EditIcon = styled(FontAwesomeIcon)`
-  position: absolute;
-  top: 40px;
-  right: 24px;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-
-  &:hover {
-    color: ${({ theme }) => theme.colors.green};
-  }
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    right: 20%;
-  }
-`;
-
-const LabelsContainer = styled.div`
-  ${({ theme }) => theme.mixins.flexCenter}
-  flex-direction: column;
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
-    padding-top: ${({ theme }) => theme.spacing(2)};
-  }
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    padding-top: ${({ theme }) => theme.spacing(4)};
-  }
-`;
-
-const TimeLabel = styled.h3`
-  padding: 0;
-  margin: 0;
-  font-size: 40px;
-  font-weight: 600;
-`;
-
-const CityLabel = styled.h6`
-  padding: 0;
-  margin: 0;
-  font-size: 16px;
-  font-weight: 300;
-  color: ${({ theme }) => theme.colors.green};
-`;
-
-const ButtonWrapper = styled.div`
-  width: 100%;
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    max-width: 30%;
-  }
-`;
-
-const LocationIcon = styled(motion.img)`
-  width: 32px;
-  height: 32px;
-  position: absolute;
-  top: -30px;
-  left: 50%;
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Tablet)} {
-    width: 40px;
-    height: 40px;
-    top: -40px;
-  }
-
-  ${({ theme }) => theme.mediaQuery(Breakpoints.Desktop)} {
-    width: 56px;
-    height: 56px;
-    top: -70px;
-  }
-`;
-
-const locationIconVariants = {
-  initial: { x: '-50%', y: -20, opacity: 0 },
-  animate: {
-    x: '-50%',
-    y: [0, -5, 0],
-    opacity: 1,
-    transition: {
-      duration: 0.7,
-      ease: 'easeInOut',
-    },
-  },
-};
+import {
+  ButtonWrapper,
+  CityLabel,
+  ClocksWrapper,
+  ClockWrapper,
+  EditIcon,
+  LabelsContainer,
+  LocationIcon,
+  locationIconVariants,
+  TimeLabel,
+  Wrapper,
+} from './Clock.styles';
 
 const Clock = () => {
   const clocks = useSelector((state: RootState) => state.timezones.clocks);
